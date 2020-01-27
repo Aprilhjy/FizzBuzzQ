39ce94e3024f0641b1dee6c5b5ed5805203297bb
April He <a.he@josari.io>
2020-01-27 16:28:39 +1000

    divisable by 3 and 5

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                string results = "";
+                if (i % 3 == 0) results = "Fizz";
+
+                if (i % 5 == 0) results = results + "Buzz";
+
+                if (results.Length == 0) results = i.ToString();
+                Console.WriteLine(results);
+            }
+        }
+    }
+}
